62a6c062fb460bb4a273cda93ee956e1117bb81f
Peter Jaros <peter.a.jaros@gmail.com>
2015-09-08 19:28:39 -0400

    lein new refactor-test

---
+++ test/refactor_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns refactor-test.core-test
+  (:require [clojure.test :refer :all]
+            [refactor-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
